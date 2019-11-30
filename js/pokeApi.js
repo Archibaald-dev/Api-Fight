49349724150db6ddc047d88f7c0fcb7b4f89fa9f
Archibaald-dev <milo.lallement@yahoo.fr>
2019-11-30 20:18:48 +0100

    ADD: name, atk and def; join 2 apis

--- js/pokeApi.js
+++ js/pokeApi.js
@@ -5,16 +5,30 @@ function getApiData() {
         const data = JSON.parse(xhr.responseText);
         console.log(data);
   
-        //const imggif = data;
+        const imgPoke = data.sprites.front_default;
+        const NamePoke = data.name;
   
-        //templateCardImg("Card Image :", imggif);
+        templatePokemonImg(NamePoke, imgPoke);
       }
     };
   
     xhr.open(
       "GET",
-      "https://pokeapi.co/api/v2/pokemon?limit=390"
+      "https://pokeapi.co/api/v2/pokemon/"+ Math.floor(Math.random() * 390)
     );
     xhr.send();
   }
   getApiData();
+
+  
+function templatePokemonImg(title, name) {
+    // var x1 = Math.floor(Math.random() * 392);
+  
+    let templatePokemonImg = document.querySelector(".col--character2");
+
+        templatePokemonImg.innerHTML +=
+        '<div class="col-sm-12 pokemon"> Pokemon : ' +
+        title +
+        ' <img onerror=this.src="img/no-image-available.png" src="' + name + '" ></div>';
+  
+  }
--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+
+  var x1 = Math.floor(Math.random() * 392);
+
 function getApiData() {
   let xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
@@ -6,8 +9,12 @@ function getApiData() {
       console.log(data);
 
       const imggif = data;
+      const CardNAME = data[x1].name;
+      const CardATK = data[x1].attack;
+      const CardLIFE = data[x1].health;
 
-      templateCardImg("Card Image :", imggif);
+      templateCardImg(imggif);
+      templateCard(CardNAME,CardATK, CardLIFE);
     }
   };
 
@@ -27,56 +34,25 @@ function getApiData() {
 }
 getApiData();
 
-function templateCardImg(title, name) {
-  // var x1 = Math.floor(Math.random() * 392);
+let templateQuerry = document.querySelector(".col--character");
 
-  let templateCardImg = document.querySelector(".col--character");
-  const names = name.length;
-  for (let x1 = 0; x1 < names; x1++) {
-    templateCardImg.innerHTML +=
+function templateCardImg(name) {
+
+  //const names = name.length;
+  //for (let x1 = 0; x1 < names; x1++) {
+    templateQuerry.innerHTML +=
       '<div class="col-sm-12 bonsoir '+ x1 +'" id=" ' +
       x1 +
-      '">' +
-      title +
-      ' <img onerror=this.src="img/no-image-available.png" id="' + x1 + '" src="' + name[x1].img + '" ></div>';
+      '"><img onerror=this.src="img/no-image-available.png" id="' + x1 + '" src="' + name[x1].img + '" ></div>';
 
-    if (name[x1].img.status == 404) {
-      document.getElementById(x1).style.display = "none";
-    }
-    name[x1].img.onerror = function() {
-      document.getElementById(x1).style.display = "none";
-    };
-  }
+ // }
 }
 
-/*
-for (let i = 0; i < names; i++) {
-  // let img = document.getElementById(name[i].img);
+function templateCard(name,atk, def) {
 
-   if (name[i].img.status == 404) {
-    // img.onerror = function () {
-     document.getElementById(name[i].img).style.display="none";
-   
- //  alert('a')}
-
-       
- 
+  templateQuerry.innerHTML +=
+        '<div class="col-sm-12">Minion : '+ name +' ATK : ' +
+        atk +
+        ' DEF : ' + def + '</div>';
+ // }
 }
-
-     templateCardImg.innerHTML +=
-       '<div class="col-sm-12" id=" ' +
-       name[i].img +
-       '">' +
-       title +
-       ' <img id="' + name[i].img + '" src="' +
-
-       name[i].img +
-       '" ></div>';
-
-       if (name[i].img.status == 404) {
-       //  img.onerror = function () {
-         document.getElementById(name[i].img).style.display="none";
-  // }
- }
-}
-}*/
